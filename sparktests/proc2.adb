d6f2490503055cdd3a6f8478388941be0ea2371f
Pierre Courtieu <Pierre.Courtieu@cnam.fr>
2014-10-17 17:24:01 +0200

    disserting in idee.txt + proc2 compleified.

--- sparktests/proc2.adb
+++ sparktests/proc2.adb
@@ -7,6 +7,7 @@ procedure proc2 (ARG: in out Integer; ARG2: in Boolean) is
       N : Integer := 3;
    begin
       ARG := ARG + N + M;
+      if ARG2 and M < 30 then ARG := 23; else ARG := ARG; end if;
    end;
 begin
    P1(M,FALSE);
